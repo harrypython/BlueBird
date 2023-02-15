bfb6aab7c09772abe77ff49a3157cdc06a3c5840
Harry Python <31451509+harrypython@users.noreply.github.com>
2023-02-15 12:15:00 -0300

    adding user_agent

--- BlueBird/BlueBird.py
+++ BlueBird/BlueBird.py
@@ -80,6 +80,10 @@ class BlueBird:
             access_token_secret=os.getenv("access_token_secret")
         )
 
+        self.client.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " \
+                                 "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                                 "Chrome/110.0.0.0 Safari/537.36"
+
         return None
 
     def get_tweet(self, tweet_id: int):
